debff4688e2ff5920d64e556309a617561123a42
Gustavo Laismann <gu.laismann2013@gmail.com>
2026-09-24 21:29:38 -0300

    style: format OCI provider configuration indentation

--- oracle/provider.tf
+++ oracle/provider.tf
@@ -1,7 +1,8 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key      = var.private_key
-  region           = var.region
+provider "oci" {  
+    tenancy_ocid = var.tenancy_ocid
+    user_ocid    = var.user_ocid
+    fingerprint  = var.fingerprint
+    private_key  = var.private_key
+    region       = var.region
+
 }
