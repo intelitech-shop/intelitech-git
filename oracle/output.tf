d8b21cd546653743e4f2090392537b0d6c4468ae
Gustavo Laismann <gu.laismann2013@gmail.com>
2026-09-24 22:02:01 -0300

    feat: update VM shape to A1 Flex with higher specs and add public IP output

--- oracle/output.tf
+++ oracle/output.tf
@@ -1,3 +1,8 @@
 output "compartment_id" {
   value = oci_identity_compartment.example_compartment.id
 }
+
+output "vm_public_ip" {
+  value       = oci_core_instance.ic_pub_vm-A.public_ip
+  description = "IP público da VM criada"
+}
